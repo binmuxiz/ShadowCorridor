dab474da4501de399e45e26fe5195e65e42aaeeb
binmuxiz <leb001122@icloud.com>
2024-07-26 14:17:11 +0900

    m,nm,nm,

--- Assets/02.Scripts/GithubTest.cs
+++ Assets/02.Scripts/GithubTest.cs
@@ -5,7 +5,7 @@ public class GithubTest : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        // github test eunbin
     }
 
     // Update is called once per frame
