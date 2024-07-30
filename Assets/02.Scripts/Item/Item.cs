adbc11352ab5c7814d72c679d265fe235c7f2b14
binmuxiz <leb001122@icloud.com>
2024-07-31 01:14:55 +0900

    Item Class

--- Assets/02.Scripts/Item/Item.cs
+++ Assets/02.Scripts/Item/Item.cs
@@ -1,18 +1,25 @@
 using UnityEngine;
 
 [System.Serializable]
-public class Item
+public class Item : MonoBehaviour
 {
-    public string name;
+    public string itemName;
     public int maxCount;
-    public GameObject prefab;
+    // public GameObject prefab;
     public Sprite image;
 
-    public Item(string name, int maxCount, GameObject prefab, Sprite image)
+    public Item(string itemName, int maxCount, Sprite image)
     {
-        this.name = name;
+        this.itemName = itemName;
         this.maxCount = maxCount;
-        this.prefab = prefab;
         this.image = image;
     }
+
+    // public Item(string name, int maxCount, GameObject prefab, Sprite image)
+    // {
+    //     this.name = name;
+    //     this.maxCount = maxCount;
+    //     this.prefab = prefab;
+    //     this.image = image;
+    // }
 }
