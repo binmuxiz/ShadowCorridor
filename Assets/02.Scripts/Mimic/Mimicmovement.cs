4848ffd56ad9d2c054a1c694c702d2170147e97b
meta00700 <spacemeta007@gmail.com>
2024-08-01 18:11:49 +0900

    .

---
+++ Assets/02.Scripts/Mimic/Mimicmovement.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class Mimic : MonoBehaviour
+{
+    // 미믹의 동작을 정의할 수 있는 코드 추가
+    // 예를 들어 이동, 공격 등
+
+    void Start()
+    {
+        // 초기화 코드
+    }
+
+    void Update()
+    {
+        // 동작 코드
+    }
+}
