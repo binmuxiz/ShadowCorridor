d66ed56322929c43ebd059e742e9a39d48dcd893
rudals0021 <rudals00210@naver.com>
2024-08-07 13:53:30 +0900

    서랍asset추가

---
+++ Assets/02.Scripts/Monster/OpenDrawer.cs
@@ -0,0 +1,79 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenDrawer : MonoBehaviour
+{
+    public Animator ANI;
+
+    public GameObject openText;
+
+    public GameObject closedText;
+
+    public AudioSource openSound;
+
+    public AudioSource closeSound;
+
+    private bool open;
+
+    private bool inReach;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        openText.SetActive(false);
+        closedText.SetActive(false);
+        
+        ANI.SetBool("open", false);
+        ANI.SetBool("close", false);
+
+        open = false;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "player" && !open)
+        {
+            inReach = true;
+            openText.SetActive(true);
+        }
+        else if (other.gameObject.tag == "player" && open)
+        {
+            inReach = true;
+            closedText.SetActive(true);
+        }
+        
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "player")
+        {
+            inReach = false;
+            openText.SetActive(false);
+            closedText.SetActive(false);
+        }
+    }
+
+    void Update()
+    {
+        if (!open && inReach && Input.GetMouseButtonDown(0))
+        {
+            openSound.Play();
+            ANI.SetBool("open", true);
+            ANI.SetBool("close", false);
+            open = true;
+            openText.SetActive(false);
+            inReach = false;
+        }
+        else if (open && inReach && Input.GetMouseButtonDown(0))
+        {
+            closeSound.Play();
+            ANI.SetBool("open", false);
+            ANI.SetBool("close", true);
+            open = false;
+            closedText.SetActive(false);
+            inReach = false;
+        }
+    }
+
+}
