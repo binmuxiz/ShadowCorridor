9a275909434313684c362d5463f84c86fdbea048
rudals0021 <rudals00210@naver.com>
2024-08-11 19:09:28 +0900

    light flicker기능..근데 좀비니갈때랑 연결x

---
+++ Assets/14.Test/Light/LightFlicker.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public class LightFlicker : MonoBehaviour
+{
+    
+    //추가해야할 것 : zombie가 지나가면 flicker가동!!
+
+    public Light lightOB;
+  
+    public float minTime;
+    public float maxTime;
+    public float timer;
+    void Start()
+    {
+        timer = Random.Range(minTime, maxTime);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        LightsFlickering();
+    }
+
+    void LightsFlickering()
+    {
+        if (timer > 0)
+            timer -= Time.deltaTime;
+
+        if (timer <= 0)
+        {
+            lightOB.enabled = !lightOB.enabled;
+            timer = Random.Range(minTime, maxTime);
+           
+        }
+    }
+    
+    
+}
