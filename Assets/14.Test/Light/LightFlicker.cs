7ddb4d5366558763811b5a02454f578724353e21
rudals0021 <rudals00210@naver.com>
2024-08-12 14:05:35 +0900

    좀비가까이 오면 좀비깜빡이는 거 기능 근데 안가까이있으면 꺼져있어야 함.

--- Assets/14.Test/Light/LightFlicker.cs
+++ Assets/14.Test/Light/LightFlicker.cs
@@ -4,23 +4,36 @@ using UnityEngine;
 public class LightFlicker : MonoBehaviour
 {
     
-    //추가해야할 것 : zombie가 지나가면 flicker가동!!
-
     public Light lightOB;
-  
+    public float triggerDistance = 5f; // 좀비가 얼마나 가까워져야 전등이 깜빡이는지 설정하는 거리
     public float minTime;
     public float maxTime;
     public float timer;
     void Start()
     {
         timer = Random.Range(minTime, maxTime);
+        lightOB.enabled = false; //시작할때 꺼져있어야 하는 거 다시 설정해야함!!
     }
 
     // Update is called once per frame
-    void Update() 
+    void Update()
     {
-        LightsFlickering();
+        GameObject[] zombies = GameObject.FindGameObjectsWithTag("Zombie");
+
+        foreach (GameObject zombie in zombies)
+        {
+            float distanceToZombie = Vector3.Distance(zombie.transform.position, transform.position);
+
+            if (distanceToZombie <= triggerDistance)
+            {
+                LightsFlickering();
+                return;
+            }
+        }
+
+        lightOB.enabled = true;
     }
+
 
     void LightsFlickering()
     {
