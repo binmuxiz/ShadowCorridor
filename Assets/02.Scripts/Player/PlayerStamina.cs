7cba078ac78da8f3c0404102e1b8d9585ab78cb7
meta00700 <spacemeta007@gmail.com>
2024-08-13 17:00:06 +0900

    스테미너 바 원작과 같은 형태로 수정 및 속도 조절

--- Assets/02.Scripts/Player/PlayerStamina.cs
+++ Assets/02.Scripts/Player/PlayerStamina.cs
@@ -3,15 +3,21 @@ using UnityEngine.UI;
 
 public class PlayerStamina : MonoBehaviour
 {
-    public Image staminaBar; // 실제 스테미너를 나타내는 UI 이미지
+    public RectTransform staminaBar;  // 실제 스테미너 바의 RectTransform
     public float maxStamina = 100f;
     private float currentStamina;
-    public float staminaUsageRate = 50f; // 스테미너 소모율
-    public float staminaRecoveryRate = 10f; // 스테미너 회복율
+    public float staminaUsageRate = 70f;  // 스태미너 소모율
+    public float staminaRecoveryRate = 10f;  // 스태미너 회복율
+
+    private float originalWidth;  // 스태미너 바의 원래 너비
 
     void Start()
     {
         currentStamina = maxStamina;
+        originalWidth = staminaBar.sizeDelta.x;  // 초기 스태미너 바의 너비 저장
+
+        // 중앙 고정을 위해 pivot을 중앙에 설정
+        staminaBar.pivot = new Vector2(0.5f, 0.5f);
         UpdateStaminaBar();
     }
 
@@ -43,6 +49,10 @@ public class PlayerStamina : MonoBehaviour
 
     private void UpdateStaminaBar()
     {
-        staminaBar.fillAmount = currentStamina / maxStamina;
+        // 스테미너 비율 계산
+        float staminaRatio = currentStamina / maxStamina;
+
+        // 스테미너 바의 너비 조정
+        staminaBar.sizeDelta = new Vector2(originalWidth * staminaRatio, staminaBar.sizeDelta.y);
     }
 }
