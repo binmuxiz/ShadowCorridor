8db65d0bd031c6f4922328f8937aac8c2f2a3b12
binmuxiz <leb001122@icloud.com>
2024-08-13 18:36:48 +0900

    총 발사 스크립트 이해중..

--- Assets/02.Scripts/Item/ItemUseController/SimpleShoot.cs
+++ Assets/02.Scripts/Item/ItemUseController/SimpleShoot.cs
@@ -5,20 +5,37 @@ using UnityEngine;
 [AddComponentMenu("Nokobot/Modern Guns/Simple Shoot")]
 public class SimpleShoot : MonoBehaviour
 {
+    // Header : Inspector창에서 변수 그룹의 제목을 정할 수 있다.
+    // SerializedField : 멤버변수가 private이라도 Inspector 창에서 편집 가능하도록 
+    // Tooltip : Inspector 창에서 변수에 마우스를 올려놓으면 설명 텍스트 제공 
+    
+    
     [Header("Prefab Refrences")]
+    // 총알 프리팹
     public GameObject bulletPrefab;
+    // 탄피 프리팹
     public GameObject casingPrefab;
+    // 총구 섬광 (발사 효과) 프리팹 
     public GameObject muzzleFlashPrefab;
 
-    [Header("Location Refrences")]
+    [Header("Location References")]
+    // 총 애니메이션 담당하는 Animator 컴포넌트
     [SerializeField] private Animator gunAnimator;
+    // 총알이 발사되는 위치를 나타냄 (보통 총구의 위치)
     [SerializeField] private Transform barrelLocation;
+    // 탄피가 배출되는 위치 
     [SerializeField] private Transform casingExitLocation;
 
     [Header("Settings")]
-    [Tooltip("Specify time to destory the casing object")] [SerializeField] private float destroyTimer = 2f;
-    [Tooltip("Bullet Speed")] [SerializeField] private float shotPower = 500f;
-    [Tooltip("Casing Ejection Speed")] [SerializeField] private float ejectPower = 150f;
+    // 생성된 탄피나 총구 섬광 효과를 몇 초 뒤에 파괴할지
+    [Tooltip("Specify time to destory the casing object")] 
+    [SerializeField] private float destroyTimer = 2f;
+    // 발사된 총알의 속도 
+    [Tooltip("Bullet Speed")] 
+    [SerializeField] private float shotPower = 500f;
+    // 탄피 배출 시 탄피에 적용되는 힘의 크기 
+    [Tooltip("Casing Ejection Speed")] 
+    [SerializeField] private float ejectPower = 150f;
 
 
     void Start()
@@ -43,15 +60,21 @@ public class SimpleShoot : MonoBehaviour
         }
     }
 
+    /**
+     * Shoot(), CasingRelease()는 애니메이션 이벤트를 통해 호출된다.
+     * 애니메이션 이벤트 : 애니메이션 클립의 특정 프레임에 메서드를 호출할 수 있는 기능
+     * 
+     */
 
     //This function creates the bullet behavior
+    // 총알 발사와 관련된 모든 행동을 수행하는 메소드 
     void Shoot()
     {
-        if (muzzleFlashPrefab)
+        Debug.Log("SimpleShoot.Shoot()");
+        if (muzzleFlashPrefab) // null 체크
         {
             //Create the muzzle flash
-            GameObject tempFlash;
-            tempFlash = Instantiate(muzzleFlashPrefab, barrelLocation.position, barrelLocation.rotation);
+            GameObject tempFlash = Instantiate(muzzleFlashPrefab, barrelLocation.position, barrelLocation.rotation);
 
             //Destroy the muzzle flash effect
             Destroy(tempFlash, destroyTimer);
@@ -67,8 +90,11 @@ public class SimpleShoot : MonoBehaviour
     }
 
     //This function creates a casing at the ejection slot
+    // 탄피 배출과 관련된 모든 행동을 수행하는 메서드 
     void CasingRelease()
     {
+        Debug.Log("SimpleShoot.CasingRelease");
+
         //Cancels function if ejection slot hasn't been set or there's no casing
         if (!casingExitLocation || !casingPrefab)
         { return; }
